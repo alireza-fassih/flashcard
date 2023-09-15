e4549ba9c175a050a1cf59cc6c7d17416aded13e
alireza.fassih <fassih.alireza@gmail.com>
2023-09-15 18:14:16 +0330

    change review view

--- scripts/reviewSession.php
+++ scripts/reviewSession.php
@@ -36,35 +36,50 @@ $xssToken = createNewXssToken();
     </head>
     <body>
         <?php include("links.php") ?>
+        <main class="container">
 
-        <div>
-            <p>Word: <?= $topWord['word']->WORD ?></p>
-            <p>correct count: <?= $topWord['word']->CORRECT_COUNT ?></p>
-            <p>wrong count: <?= $topWord['word']->WRONG_COUNT ?></p>
-            <p id="meaning" style="visibility: hidden;">meaning: <?= $topWord['word']->MEANING ?></p>
-        </div>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title"><?= $topWord['word']->WORD ?></h5>
+                    <p class="card-text">
+                        correct count: <?= $topWord['word']->CORRECT_COUNT ?>
+                        -
+                        wrong count: <?= $topWord['word']->WRONG_COUNT ?>
+                    </p>
+                    <p class="card-text" id="meaning" style="visibility: hidden;">'
+                        <?= $topWord['word']->MEANING ?>
+                    </p>
+                </div>
 
-        <a href="#" onclick="showMeaning();">show meaning</a>
-        <br />
-        <br />
-        <br />
+                <div class="card-body">
+                    <a href="#" class=" btn btn-primary btn-lg" onclick="showMeaning();">show meaning</a>
+                </div>
 
-        <form method="post" >
-            <input type="hidden" name="word" value="<?= $topWord['word']->ID ?>" />
-            <input type="hidden" name="ws" value="<?= $topWord['info']->ID ?>" /> 
-            <input type="hidden"  name="xssToken" value="<?= $xssToken ?>"/>
-            <button type="submit" name="ok" value="true" >ok</button>
-        </form>
+                <div class="card-body">
+                    <form method="post" >
+                        <input  type="hidden" name="word" value="<?= $topWord['word']->ID ?>" />
+                        <input  type="hidden" name="ws" value="<?= $topWord['info']->ID ?>" /> 
+                        <input  type="hidden"  name="xssToken" value="<?= $xssToken ?>"/>
+                        <button type="submit" class="btn btn-success btn-lg" name="ok" value="true" >ok</button>
+                    </form>
+                </div>
 
-        <br />
+                <div class="card-body">
+                    <form method="post" >
+                        <input  type="hidden" name="word" value="<?= $topWord['word']->ID ?>" />
+                        <input  type="hidden" name="ws" value="<?= $topWord['info']->ID ?>" /> 
+                        <input  type="hidden" name="xssToken" value="<?= $xssToken ?>"/>
+                        <button type="submit" class="btn btn-danger btn-lg" name="no" value="true" >no</button>
+                    </form>
+                </div>
+            </div>
 
-        <form method="post" >
-            <input type="hidden" name="word" value="<?= $topWord['word']->ID ?>" />
-            <input type="hidden" name="ws" value="<?= $topWord['info']->ID ?>" /> 
-            <input type="hidden"  name="xssToken" value="<?= $xssToken ?>"/>
-            <button type="submit" name="no" value="true" >no</button>
-        </form>
+            
 
+            <br />
+
+            
+        </main>
     </body>
     <script  type="text/javascript">
         function showMeaning() {
