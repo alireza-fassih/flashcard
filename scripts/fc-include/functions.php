70ce5e43f0bb8b4fb897e820d06120a150f6ac03
alireza.fassih <fassih.alireza@gmail.com>
2023-09-22 02:59:21 +0330

    extract all functions inside functiona

---
+++ scripts/fc-include/functions.php
@@ -0,0 +1,51 @@
+<?php
+
+
+
+function redirectToLoginInUserNotLoggedIn() {
+    if (!isset($_SESSION[USER_ID_SESSION_KEY])) {
+        redirectTo("/login.php");
+    }
+}
+
+
+function redirectTo($loc) {
+    header("location: $loc");
+    exit();
+}
+
+function createNewXssToken() {
+    $rand = bin2hex(random_bytes(10));
+    $_SESSION['xssToken'] = $rand;
+    return $rand;
+}
+
+function getXssToken() {
+    $token = @$_SESSION['xssToken'];
+    unset($_SESSION['xssToken']);
+    return $token;
+}
+
+
+function printNotifications() {
+    global $_NOTIFICATIONS;
+    foreach ($_NOTIFICATIONS as $n) {
+        echo "<div class='alert alert-" . $n['level'] . "' role='alert'>" . $n['message'] . "</div>";
+    }
+}
+
+function addSuccessNotification($msg) {
+    global $_NOTIFICATIONS;
+    $_NOTIFICATIONS[] = array("level" => "success", "message" => $msg);
+}
+
+function addErrorNotification($msg) {
+    global $_NOTIFICATIONS;
+    $_NOTIFICATIONS[] = array("level" => "danger", "message" => $msg);
+}
+
+
+function captureDbError() {
+    global $DB;
+    addErrorNotification("database error: " . $DB->error);
+}
--- scripts/fc-include/include.php
+++ scripts/fc-include/include.php
@@ -1,65 +1,17 @@
 <?php
 
-const USER_ID_SESSION_KEY = "user_id";
 
+require_once(__DIR__ . '/functions.php');
 require_once( __DIR__ . '/userdao.php');
 require_once( __DIR__ . '/word.php');
 require_once(__DIR__ . '/sessiondao.php');
+
+const USER_ID_SESSION_KEY = "user_id";
 
 session_start();
 
 $_NOTIFICATIONS = array();
 $_VIEW_DATA = array();
-
-
-function redirectToLoginInUserNotLoggedIn() {
-  if(!isset($_SESSION[ USER_ID_SESSION_KEY ])) {
-    redirectTo("/login.php");
-  }
-}
-
-
-function redirectTo( $loc ) {
-  header("location: $loc");
-  exit();
-}
-
-function createNewXssToken() {
-  $rand = bin2hex(random_bytes(10)); 
-  $_SESSION['xssToken'] = $rand;
-  return $rand;
-}
-
-function getXssToken() {
-  $token = @$_SESSION['xssToken'];
-  unset($_SESSION['xssToken']);
-  return $token;
-}
-
-
-function printNotifications() {
-  global $_NOTIFICATIONS;
-  foreach($_NOTIFICATIONS as $n){
-    echo "<div class='alert alert-" . $n['level'] . "' role='alert'>" . $n['message'] . "</div>";
-  }
-}
-
-function addSuccessNotification($msg) {
-  global $_NOTIFICATIONS;
-  $_NOTIFICATIONS[] = array("level"=>"success", "message"=> $msg);
-}
-
-function addErrorNotification($msg) {
-  global $_NOTIFICATIONS;
-  $_NOTIFICATIONS[] = array("level"=>"danger", "message"=> $msg);
-}
-
-
-function captureDbError() {
-  global $DB;
-  addErrorNotification("database error: " . $DB->error);
-}
-
 
 
 $_CONFIG = parse_ini_file( __DIR__ . '/app.env');
